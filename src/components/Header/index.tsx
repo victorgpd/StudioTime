12886c31ec4b9f86fb94d87b3e73a65b72af930e
victorgpd <victorguilherme2407@outlook.com>
2025-06-20 09:22:15 -0300

    remoção do padding no main e dando função para a logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { Container, Logo } from "./styles";
+import { RoutesEnum } from "../../enums/routes";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
-      <Logo src="/images/logo-horizontal.png" alt="Logo StudioTime" />
+      <Logo src="/images/logo-horizontal.png" alt="Logo StudioTime" onClick={() => navigate(RoutesEnum.Home)} />
     </Container>
   );
 };
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   flex: 1;
-  padding: 15px;
 
   background-color: #fbfafc;
 `;
