a151cea0e1aecc31feadd9f75e6ca7e8cef8604f
victorgpd <victorguilherme2407@outlook.com>
2025-06-24 09:52:33 -0300

    modificando ContainerPage para ficar mais dinamico a sua utilização

--- src/components/ContainerPage/index.tsx
+++ src/components/ContainerPage/index.tsx
@@ -1,7 +1,18 @@
 import { Container } from "./styles";
 
-const ContainerPage = ({ children }: { children: React.ReactNode }) => {
-  return <Container>{children}</Container>;
+interface ContainerPageProps {
+  children: React.ReactNode;
+  gap?: string
+  alignItems?: "flex-start" | "center" | "flex-end" | undefined;
+  justifyContent?: "flex-start" | "space-between" | "space-around" | "space-evenly" | "center" | "flex-end" | undefined;
+}
+
+const ContainerPage = ({ children, gap, alignItems, justifyContent }: ContainerPageProps) => {
+  return (
+    <Container gap={gap} alignItems={alignItems} justifyContent={justifyContent}>
+      {children}
+    </Container>
+  );
 };
 
 export default ContainerPage;
--- src/components/ContainerPage/styles.ts
+++ src/components/ContainerPage/styles.ts
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+interface ContainerProps {
+  gap?: string;
+  alignItems?: "flex-start" | "center" | "flex-end" | undefined;
+  justifyContent?: "flex-start" | "space-between" | "space-around" | "space-evenly" | "center" | "flex-end" | undefined;
+}
+
+export const Container = styled.div<ContainerProps>`
   width: 100%;
   height: 100%;
 
   padding: 15px;
+
+  ${(props) => `gap: ${props.gap}`};
+  display: flex;
+  ${(props) => `align-items: ${props.alignItems}`};
+  ${(props) => `justify-content: ${props.justifyContent}`};
 `;
