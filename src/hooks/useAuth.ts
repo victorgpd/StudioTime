5cf7be36d1847836f85c3ee284c3c077279e327d
victorgpd <victorguilherme2407@outlook.com>
2025-06-26 13:25:22 -0300

    criação das funções de login, register e logout

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,57 @@
+import type { IUser } from "../enums/types";
+import type { NavigateFunction } from "react-router-dom";
+
+import { useState } from "react";
+import { auth } from "../services/firebase";
+import { RoutesEnum } from "../enums/routes";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+
+export const useAuth = (navigate: NavigateFunction) => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const runAsync = async <T>(fn: () => Promise<T>): Promise<T | null> => {
+    setLoading(true);
+    setError("");
+
+    try {
+      return await fn();
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Erro desconhecido.");
+      }
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const login = (email: string, password: string) =>
+    runAsync(async () => {
+      await signInWithEmailAndPassword(auth, email, password);
+
+      navigate(RoutesEnum.Painel);
+      return null;
+    });
+
+  const register = (user: IUser, password: string) =>
+    runAsync(async () => {
+      const userCredential = await createUserWithEmailAndPassword(auth, user.email, password);
+
+      await updateProfile(userCredential.user, {
+        displayName: user.name,
+      });
+
+      navigate(RoutesEnum.Painel);
+      return userCredential.user;
+    });
+
+  const logout = () =>
+    runAsync(async () => {
+      await signOut(auth);
+    });
+
+  return { login, register, logout, error, loading };
+};
